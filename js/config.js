4f0e9aa6bdd5ac97b9ba8495cc14344728ace62f
Nolstan Kumwenda <nolstankumwenda@gmail.com>
2026-01-15 20:31:36 +0200

    commit added render url

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'https://market-place-q0q5.onrender.com/api';
+    : 'https://localmarket-uwjd.onrender.com/api';
